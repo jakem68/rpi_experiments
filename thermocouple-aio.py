12d9ffc0e092b25c88c0400b68daf1ebb725f798
Jan Kempeneers <jakem68@users.noreply.github.com>
2019-01-20 22:24:43 +0100

    added opcua server in thermocouple-aio.py

--- thermocouple-aio.py
+++ thermocouple-aio.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# Copyright (c) 2014 Adafruit Industries - Author: Tony DiCola
 
-# Can enable debug output by uncommenting:
-#import logging
-#logging.basicConfig(level=logging.DEBUG)
 
 import time
-
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MAX31855.MAX31855 as MAX31855
-# Import library and create instance of REST client.
 from Adafruit_IO import Client
+import sys
+sys.path.insert(0, "..")
+from opcua import ua, Server
+
+# Configuration of Adafruit-IO Client and feed.
 aio = Client('jankempeneers', '0642323bbbf34022955dd1f38024dfce')
 oven_temperature = aio.feeds('sirris10-amifv2.sirris10-amifv2-oven-temperature')
-# interval at which data will be sent to Adafruit in seconds
-interval = 60
 
+# Configuration of Raspberry Pi software SPI.
+CLK = 25
+CS  = 24
+DO  = 18
+sensor = MAX31855.MAX31855(CLK, CS, DO)
 
-# Define a function to convert celsius to fahrenheit.
+# Interval at which data will be sent to Adafruit and opcua-server in seconds.
+interval_adafruit = 300
+interval_opcua = 1
+interval_errorwait = 10
+interval_programloop = 0.1
+adafruit_send_retries = 10
+
+programloops_per_adafruit = interval_adafruit/interval_programloop
+programloops_per_opcua = interval_opcua/interval_programloop
+counter_programloops_adafruit = 0
+counter_programloops_opcua = 0
+
+# A function to convert celsius to fahrenheit.
 def c_to_f(c):
     return c * 9.0 / 5.0 + 32.0
 
@@ -45,24 +41,53 @@ def isNaN(num):
     return num != num
 
 
-# Uncomment one of the blocks of code below to configure your Pi or BBB to use
-# software or hardware SPI.
+# A function to start opcua server (is blocking must be run in separate thread).
+def start_opcua_server():
+    # setup our server
+    server = Server()
+    server.set_endpoint("opc.tcp://0.0.0.0:4840/sirris/server/")
 
-# Raspberry Pi software SPI configuration.
-CLK = 25
-CS  = 24
-DO  = 18
-sensor = MAX31855.MAX31855(CLK, CS, DO)
+    # setup our own namespace, not really necessary but should as spec
+    uri = "http://examples.sirris.github.io"
+    idx = server.register_namespace(uri)
 
-# Loop printing measurements every second.
-print('Press Ctrl-C to quit.')
-while True:
-    temp = sensor.readTempC()
-    internal = sensor.readInternalC()
-#    print('Thermocouple Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
-#    print('    Internal Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, c_to_f(internal)))
-    print('Thermocouple Temperature: {0:0.3F}*C'.format(temp))
-    # Add the value 98.6 to the feed 'Temperature'.
+    # get Objects node, this is where we should put our nodes
+    objects = server.get_objects_node()
+
+    # populating our address space
+    myobj = objects.add_object(idx, "AMIF_oven")
+    myvar = myobj.add_variable(idx, "oven_temperature", 6.7)
+    myvar.set_writable()    # Set MyVariable to be writable by clients
+    # q.put(myvar)
+
+    # starting!
+    server.start()
+
+    return myvar
+
+def time_to_inform_opcua():
+    global counter_programloops_opcua
+    global programloops_per_opcua
+    if counter_programloops_opcua > programloops_per_opcua:
+        counter_programloops_opcua = 0
+        return True
+    else:
+        print('counter programloops opcua is {}'.format(counter_programloops_opcua))
+        counter_programloops_opcua += 1
+        return False
+
+def time_to_inform_adafruit():
+    global counter_programloops_adafruit
+    global programloops_per_adafruit
+    if counter_programloops_adafruit > programloops_per_adafruit:
+        counter_programloops_adafruit = 0
+        return True
+    else:
+        print('counter programloop adafruit is {}'.format(counter_programloops_adafruit))
+        counter_programloops_adafruit += 1
+        return False
+
+def inform_adafruit(temp):
     if not isNaN(temp):
         try:
             aio.send_data(oven_temperature.key, temp)
@@ -70,10 +95,33 @@ while True:
         except:
             print("Some error, let's wait a little longer")
             errorcount += 1
-            time.sleep(10.0)
-            if errorcount > 10:
+            time.sleep(interval_errorwait)
+            if errorcount > adafruit_send_retries:
                 print("error has occured more than 10 times, I am quitting")
-                break
             pass
 
-    time.sleep(interval)
+def inform_opcua(myvar, temp):
+    myvar.set_value(temp)
+
+
+def main():
+    # q = Queue.Queue()
+    # thread1 = threading.Thread(target=start_opcua_server, args=(q))
+    # thread1.start()
+    # time.sleep(0.1)
+    # myvar = q.get()
+    myvar = start_opcua_server()
+    while True:
+        temp = sensor.readTempC()
+        internal = sensor.readInternalC()
+        print('Thermocouple Temperature: {0:0.3F}*C'.format(temp))
+        if time_to_inform_adafruit():
+            inform_adafruit(temp)
+        
+        if time_to_inform_opcua():
+            inform_opcua(myvar, temp)
+
+        time.sleep(interval_programloop)
+
+if __name__ == "__main__":
+    main()
